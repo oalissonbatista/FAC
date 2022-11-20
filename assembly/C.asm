ae3dd942202c0d46643a33bbaa29d9e6a63c3bfd
Alisson Batista <alisson252525@hotmail.com>
2022-11-20 13:28:50 -0300

    Atualizaçaõ de código

--- assembly/C.asm
+++ assembly/C.asm
@@ -1,10 +1,26 @@
 .data
-message: .asciiz "\n"
+pular_linha: .asciiz "\n"
 .text
 main:
-    li $v0,4
-    la $a0, message
+    li $v0,5
+    syscall
+    
+    move $t0,$v0
+
+    li $v0,5
     syscall
 
-    li $v0,10
+    move $t1, $v0
+
+    sub $t2,$t0,$t1
+
+    li $v0, 1
+    move $a0, $t2
+    syscall
+
+    li $v0, 4 
+    la $a0, pular_linha
+    syscall
+
+    li $v0, 10
     syscall
